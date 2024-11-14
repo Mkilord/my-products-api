353a970d54be1792f766c5f4a7a3c6a82d213e84
Mkilord <mkilord6666@gmail.com>
2024-11-14 16:19:16 +0300

    Add JpaSpecificationExecutor

--- src/main/java/ru/romashkaco/myproducts/repository/ProductRepository.java
+++ src/main/java/ru/romashkaco/myproducts/repository/ProductRepository.java
@@ -1,13 +1,14 @@
 package ru.romashkaco.myproducts.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.romashkaco.myproducts.model.Product;
 
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     @Modifying
     @Query("DELETE FROM Product p WHERE p.id = :id")
     int deleteByIdAndReturnCount(@Param("id") Long id);
